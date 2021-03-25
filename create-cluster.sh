54728c9a634162125e0fdfe838ca897788204570
Andy Gore <64426235+drbungle@users.noreply.github.com>
2021-03-25 01:08:32 +0000

    ACR import

--- create-cluster.sh
+++ create-cluster.sh
@@ -1,8 +1,19 @@
 az group create --name k8sDemoResourceGroup --location westeurope
 
+ACR=demoacr$RANDOM
+
+az acr create -n $ACR -g k8sDemoResourceGroup --sku basic
+
 az aks create --resource-group k8sDemoResourceGroup --name myAksCluster --node-count 1 --enable-addons monitoring --generate-ssh-keys
 
 az aks install-cli
 
 az aks get-credentials --resource-group k8sDemoResourceGroup --name myAksCluster
+
+demoacr1480.azurecr.io
+
+
+az aks update -n myAksCluster -g k8sDemoResourceGroup --attach-acr demoacr1480
+
+ az acr login -n demoacr1480.azurecr.io
 
